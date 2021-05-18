7690d9c1fd9a7272b5b84a7547eb24acead6b504
Saad <saad.patel@thinksense.ai>
2021-05-18 15:50:29 +0530

    init

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_catalog_app/pages/home_page.dart';
+import 'package:flutter_catalog_app/pages/login_page.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+      ),
+      routes: {
+        "/": (context) => LoginPage(),
+        "/login": (context) => LoginPage(),
+        "/home": (context) => HomePage(),
+      },
+    );
+  }
+}
---
+++ lib/pages/login_page.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget {
+  @override
+  LoginPageState createState() => LoginPageState();
+}
+
+class LoginPageState extends State<LoginPage> {
+  bool btnClicked = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      child: Center(
+        // child: ElevatedButton(
+        //   child: Text("Login"),
+        //   style: TextButton.styleFrom(minimumSize: Size(150, 40)),
+        //   onPressed: () {
+        //     Navigator.pushNamed(context, "/home");
+        //   },
+        // ),
+        child: InkWell(
+          onTap: () async {
+            setState(() {
+              btnClicked = true;
+            });
+            await Future.delayed(Duration(milliseconds: 200));
+            Navigator.pushNamed(context, "/home");
+          },
+          child: AnimatedContainer(
+            duration: Duration(milliseconds: 200),
+            width: btnClicked ? 50 : 150,
+            height: 50,
+            alignment: Alignment.center,
+            child: btnClicked
+                ? Icon(
+                    Icons.done,
+                    color: Colors.white,
+                  )
+                : Text(
+                    "Login",
+                    style: TextStyle(color: Colors.white),
+                  ),
+            decoration: BoxDecoration(
+                color: Colors.green,
+                borderRadius: btnClicked
+                    ? BorderRadius.circular(50)
+                    : BorderRadius.circular(8)),
+          ),
+        ),
+      ),
+    );
+  }
+}
